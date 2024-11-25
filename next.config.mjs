f5a3c55f278111c3a125457b6998f747ff591900
Juan Alejandro <juanalejocardenas@unicauca.edu.co>
2024-11-24 19:06:02 -0500

    Add SVG assets and update Next.js configuration for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    basePath: '/wave-simulator',
+    images: {
+      unoptimized: true
+    }
+  }
+  
+  // Change module.exports to export default for .mjs files
+  export default nextConfig
